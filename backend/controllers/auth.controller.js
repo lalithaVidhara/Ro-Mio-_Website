e70055ca5f62c39ece117718cd3cd4151e8893c2
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-08 22:31:47 +0530

    creating auth db with redis, cookies and tokens

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,37 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken"; 
+
+const generateTokens = (userId) => {
+    const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+    })
+
+    const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d",
+    })
+
+    return { accessToken, refreshToken };
+}
+
+const storeRefreshToken = async (refreshToken, userId) => {
+    await redis.set(`refreshToken:${userId}`, refreshToken, "EX", 7*24*60*60); //7days
+} 
+
+const setCookies = (res, accessToken, refreshToken) => {
+   res.cookie("accessToken", accessToken, {
+       httpOnly: true, //Prevent XSS attacks
+       secure:process.env.NODE_ENV === "production", //Only send cookie over HTTPS
+       sameSite: "strict", //Prevent CSRF attacks
+       maxAge: 15 * 60 * 1000, //15 minutes
+   }); 
+   res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, //Prevent XSS attacks
+    secure:process.env.NODE_ENV === "production", //Only send cookie over HTTPS
+    sameSite: "strict", //Prevent CSRF attacks
+    maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
+   }); 
+};
 
 export const signup = async (req, res) => {
     const { name, email, password } = req.body;
@@ -9,14 +42,25 @@ export const signup = async (req, res) => {
         }    
         const user = await User.create({ name, email, password });
 
-        res.status(201).json({user, message: "User created successfully"});
+        //authentication
+        const {accessToken, refreshToken} = generateTokens(user._id);
+        await storeRefreshToken(user._id, refreshToken);
+
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+        }, message: "User created successfully"});
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+};
 export const login = async (req, res) => {
     res.send("login route called");
-}
+};
 export const logout = async (req, res) => {
     res.send("logout route called");
-}
+};
