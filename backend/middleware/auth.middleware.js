3b13c00b566a801dad123a7cec94eaa99846d16a
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-14 23:48:24 +0530

    create middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,7 +1,32 @@
-export const protectRoute = (req, res, next) => {
+import jwt from "jsonwebtoken";
+
+export const protectRoute = async (req, res, next) => {
     try {
         const accessToken = req.cookies.accessToken;
+
+        if (!accessToken) {
+            return res.status(401).json({ message: "Unauthorized - No Access Token" });
+        }
+
+        try {
+            const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+            const user = await User.findById(decoded.userId).select("-password");
+
+            if (!user) {
+                return res.status(401).json({ message: "User Not Found" });
+            }
+
+            req.user = user;
+            next();
+        } catch (error) {
+            if(error.name === "TokenExpiredError") {
+                return res.status(401).json({ message: "Unauthorized - Access Token Expired" });
+            }
+            throw error;
+        }
+
     } catch (error) {
-        
+        console.log("Error in protectRoute middleware", error.message);
+        res.status(401).json({ message: "Unauthorized - Invalid Access Token" });
     }
 }
