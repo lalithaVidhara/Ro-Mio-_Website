df3469eb8ec4784fad1f6e009f72d8952fed32c3
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-16 02:25:38 +0530

    Fixed Minor Code Bug relating to Server.js file

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 import { connectDB } from "./lib/db.js";
 
