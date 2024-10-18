e5dddea5dac0640cceecde695fdd0b501bef0862
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-18 23:02:32 +0530

    create getDailySalesData Function

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,3 +1,4 @@
+import Order from "../models/order.model.js";
 import Product from "../models/product.model.js";
 import User from "../models/user.model.js";
 
@@ -5,8 +6,19 @@ export const getAnalytics = async (req, res) => {
     try {
         const analyticsData = await getAnalyticsData();
         res.json(analyticsData);
+
+        const endDate = new Date();
+        const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000); // 7 days in milliseconds
+
+        const dailySalesData = await getDailySalesData(startDate, endDate);
+
+        res json ({
+            analyticsData,
+            dailySalesData
+        })
     } catch (error) {
-        
+        console.log("Error in getAnalytics controller", error.message);
+        res.status(500).json({ message: "Server Error", error: error.message });
     }
 };
 
@@ -14,6 +26,73 @@ export const getAnalyticsData = async () => {
     const totalUsers = await User.countDocuments();
     const totalProducts = await Product.countDocuments();
 
-    //to do
-    //const salesData = 
-}
+    const salesData = await Order.aggregate([
+        {
+            $group: {
+                _id: null,
+                totalSales: { $sum: 1 },
+                totalRevenue: { $sum: "$totalAmount" },
+            },
+        },
+    ]);
+
+    const { totalSales, totalRevenue } = salesData[0] || {
+        totalSales: 0,
+        totalRevenue: 0,
+    };
+
+    return {
+        users: totalUsers,
+        products: totalProducts,
+        totalSales,
+        totalRevenue,
+    };
+};
+
+export const getDailySalesData = async (startDate, endDate) => {
+    try {
+		const dailySalesData = await Order.aggregate([
+			{
+				$match: {
+					createdAt: {
+						$gte: startDate,
+						$lte: endDate,
+					},
+				},
+			},
+			{
+				$group: {
+					_id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+					sales: { $sum: 1 },
+					revenue: { $sum: "$totalAmount" },
+				},
+			},
+			{ $sort: { _id: 1 } },
+		]);
+
+		// example of dailySalesData
+		// [
+		// 	{
+		// 		_id: "2024-08-18",
+		// 		sales: 12,
+		// 		revenue: 1450.75
+		// 	},
+		// ]
+
+		const dateArray = getDatesInRange(startDate, endDate);
+		// console.log(dateArray) // ['2024-08-18', '2024-08-19', ... ]
+
+		return dateArray.map((date) => {
+			const foundData = dailySalesData.find((item) => item._id === date);
+
+			return {
+				date,
+				sales: foundData?.sales || 0,
+				revenue: foundData?.revenue || 0,
+			};
+		});
+	} catch (error) {
+		throw error;
+	}
+};
+
