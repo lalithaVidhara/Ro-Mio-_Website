f8be503b6aff62ef7f70a91c4fbcaaddad35c414
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-18 23:25:41 +0530

    Finish backend api

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -4,6 +4,7 @@ import { getAnalytics } from "../controllers/analytics.controller.js";
 
 const router = express.Router();    
 
+
 router.get("/",protectRoute, adminRoute, getAnalytics);
 
 export default router
