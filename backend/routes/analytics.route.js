dc7260dc9783457e9fb21263f617d3df03e7c690
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-18 23:33:03 +0530

    Create getProfile function

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -4,7 +4,6 @@ import { getAnalytics } from "../controllers/analytics.controller.js";
 
 const router = express.Router();    
 
-
 router.get("/",protectRoute, adminRoute, getAnalytics);
 
 export default router
