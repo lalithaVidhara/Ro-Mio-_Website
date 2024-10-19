1667b833a1d80de3dfd266eab3b2410f64a65bce
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-19 17:38:14 +0530

    create front end setup and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
+function App() {
   return (
     <div>
-      Hello World !
+      <Routes>
+        <Route path="/" element ={<HomePage/>} /> 
+        <Route path="/signup" element ={<SignUpPage/>} /> 
+        <Route path="/login" element ={<LoginPage/>} /> 
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
