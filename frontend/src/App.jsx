79bd88ba5d90a222c5b2f6742acec04a9300c6c6
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-19 17:54:17 +0530

    create navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,12 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/" element ={<HomePage/>} /> 
         <Route path="/signup" element ={<SignUpPage/>} /> 
