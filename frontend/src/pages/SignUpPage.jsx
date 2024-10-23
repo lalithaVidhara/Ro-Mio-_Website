4c0be00c342f624b14e4b7bce2c4336a47e8e2d3
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-10-24 02:29:36 +0530

    initialize signup page creation

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {userPlus, Mail, Lock, User, ArrowRight, Loader} from 'lucide-react';
+import {motion} from 'framer-motion';
 
 const SignUpPage = () => {
+  const loading = true;
   return <div>SignUpPage</div>;
 
 };
