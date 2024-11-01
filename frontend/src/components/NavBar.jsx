4afe4d976d390020ae64857bbab7ad1601040e4a
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-11-02 02:18:27 +0530

    create logout function

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 import { useUserStore } from '../stores/useUserStore';
 
 const NavBar = () => {
-  const {user} = useUserStore(); 
-  const isAdmin = user.role === "admin"; 
+  const {user, logout} = useUserStore(); 
+  const isAdmin = user?.role === "admin"; 
   return (
 
     <header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-rose-800'>
@@ -42,7 +42,7 @@ const NavBar = () => {
           {user ? (
             <button className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
             rounded-md flex items-center transition duration-300 ease-in-out'
-            onClick={""}>
+            onClick={logout}>
               <LogOut size={18}/>
               <span className='hidden sm:inline ml-2'>Log out</span>
 
