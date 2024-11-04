ad322db86df52c9f703796d83fb2427089cdbc84
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-11-04 05:56:32 +0530

    add place holder to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+//to do add real pictures thee are place holders
+const categories = [
+	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+	{ href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+	{ href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+	{ href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
+	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
+	{ href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
+	{ href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+];
+
 const HomePage = () => {
   return <div>HomePage</div>;
   
