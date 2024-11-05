701a3ce54905bdbc2e8f8ff8cef9f81a0d46fc22
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-11-05 20:57:24 +0530

    fixed minor bugs

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,11 +16,11 @@ const HomePage = () => {
   return (
 	<div className='relative min-h-screen text-white overflow-hidden'>
 			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
-				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
-					Explore Our Categories
+				<h1 className='text-center text-5xl sm:text-6xl font-bold text-rose-400 mb-4'>
+					Explore Our Collection
 				</h1>
 				<p className='text-center text-xl text-gray-300 mb-12'>
-					Discover the latest trends in eco-friendly fashion
+					Discover the inner Romio with in YOU
 				</p>
 
 				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
@@ -29,7 +29,6 @@ const HomePage = () => {
 					))}
 				</div>
 
-				{!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
 			</div>
 		</div>
   );
