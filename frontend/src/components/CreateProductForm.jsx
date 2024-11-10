5e67ac5c115ca86cccd24239b2bf3345dc3d9555
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-11-10 20:52:46 +0530

    created CreateProductForm component

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { PlusCircle, Upload, Loader } from "lucide-react";
+
+const categories = ["jeans", "t-shirts", "shoes", "glasses", "jackets", "suits", "bags"];
 
 const CreateProductForm = () => {
-  return (
-    <div>CreateProductForm</div>
-  )
+  const [newProduct, setNewProduct] = useState({
+		name: "",
+		description: "",
+		price: "",
+		category: "",
+		image: "",
+	});
 }
 
 export default CreateProductForm
