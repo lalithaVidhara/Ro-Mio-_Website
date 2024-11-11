adef5fcbda39864f179d0d46470c74b5dfa8097b
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-11-12 00:25:53 +0530

    finish CreateProductForm component ui

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -19,6 +19,8 @@ const CreateProductForm = () => {
 		console.log(newProduct);
 	};
 
+	const loading = false;
+
 return (
 	<motion.div
 		className='bg-gray-800 shadow-lg rounded-lg p-8 mb-8 max-w-xl mx-auto'
@@ -26,18 +28,120 @@ return (
 		animate={{ opacity: 1, y: 0 }}
 		transition={{ duration: 0.8 }}
 	>
-		<h2 className='text-2xl font-semibold mb-6 text-rose-300'>Create New Product</h2>
+		<h2 className='text-2xl font-semibold mb-6 text-rose-300 text-center'>Create New Product</h2>
 
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit} className='space-y-5'>
+			<div>
+				<label htmlFor='name' className='block text-sm font-medium text-gray-300'>
+						Product Name
+				</label>
+				<input
+					type='text'
+					id='name'
+					name='name'
+					value={newProduct.name}
+					onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+					className='mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2
+					px-3 text-white focus:outline-none focus:ring-2
+					focus:ring-rose-500 focus:border-rose-500'
+					required
+				/>
+			</div>
+
+			
+			<div>
+				<label htmlFor='description' className='block text-sm font-medium text-gray-300'>
+					Description
+				</label>
+				<textarea
+					id='description'
+					name='description'
+					value={newProduct.description}
+					onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
+					rows='3'
+					className='mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm
+					py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-rose-500 
+					focus:border-rose-500'
+					required
+				/>
+			</div>
+
+			<div>
+				<label htmlFor='price' className='block text-sm font-medium text-gray-300'>
+					Price
+				</label>
+				<input
+					type='number'
+					id='price'
+					name='price'
+					value={newProduct.price}
+					onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+					step='100'
+					className='mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm 
+					py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-rose-500
+					focus:border-rose-500'
+					required
+				/>
+			</div>
+
+			<div>
+				<label htmlFor='category' className='block text-sm font-medium text-gray-300'>
+					Category
+				</label>
+				<select
+					id='category'
+					name='category'
+					value={newProduct.category}
+					onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })}
+					className='mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md
+					shadow-sm py-2 px-3 text-white focus:outline-none 
+					focus:ring-2 focus:ring-rose-500 focus:border-rose-500'
+					required
+				>
+				<option value=''>Select a category</option>
+					{categories.map((category) => (
+						<option key={category} value={category}>
+							{category}
+						</option>
+					))}
+				</select>
+			</div>
 
 
+			<div className='mt-1 flex items-center'>
+				<input type='file' id='image' className='sr-only' accept='image/*' />
+				<label
+					htmlFor='image'
+					className='cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500'
+				>
+				<Upload className='h-5 w-5 inline-block mr-2' />
+					Upload Image
+				</label>
+				{newProduct.image && <span className='ml-3 text-sm text-gray-400'>Image uploaded </span>}
+			</div>
 
+			
+			<button
+				type='submit'
+				className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md 
+				shadow-sm text-sm font-medium text-white bg-rose-600 hover:bg-rose-700 
+				focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500 disabled:opacity-50'
+				disabled={loading}
+			>
+				{loading ? (
+					<>
+						<Loader className='mr-2 h-5 w-5 animate-spin' aria-hidden='true' />
+						Loading...
+					</>
+				) : (
+					<>
+						<PlusCircle className='mr-2 h-5 w-5' />
+						Create Product
+					</>
+				)}
+			</button>
 
 		</form>
-
-
-
-
 
 	</motion.div>
 )
