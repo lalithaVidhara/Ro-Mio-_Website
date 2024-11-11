15ecc8d8070ce7a54c93604dc9bb709a1cdcc956
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2024-11-11 23:25:32 +0530

    create placeholder handle submit function for createproductform component

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -13,6 +13,34 @@ const CreateProductForm = () => {
 		category: "",
 		image: "",
 	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(newProduct);
+	};
+
+return (
+	<motion.div
+		className='bg-gray-800 shadow-lg rounded-lg p-8 mb-8 max-w-xl mx-auto'
+		initial={{ opacity: 0, y: 20 }}
+		animate={{ opacity: 1, y: 0 }}
+		transition={{ duration: 0.8 }}
+	>
+		<h2 className='text-2xl font-semibold mb-6 text-rose-300'>Create New Product</h2>
+
+		<form onSubmit={handleSubmit}>
+
+
+
+
+		</form>
+
+
+
+
+
+	</motion.div>
+)
 }
 
 export default CreateProductForm
