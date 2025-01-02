bea2d4227f9002af6ce1b6eac2750344ca07e723
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2025-01-03 01:26:38 +0530

    Finish creating Dashbooard functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className='min-h-screen bg-gray-900 text-white relative overflow-hidden'>
       {/* Background gradient */}
-			<div className='absolute inset-0 overflow-hidden'>
+			<div className='absolute inset-0 h-screen overflow-hidden'>
 				<div className='absolute inset-0'>
 					<div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(237, 47, 145, 0.8)_0%,rgba(86,27,58,0.8)_45%,rgba(0,0,0,0.1)_100%)]' />
 			  </div>
